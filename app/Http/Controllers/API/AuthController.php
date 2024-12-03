feed925a67911d44fd76a381e1ce1aa0b9af5b5b
ilhamhafidz404 <xxspanzie@gmail.com>
2024-12-03 14:51:02 +0700

    ICF-21: API Registation for Student (api)

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller
 {
@@ -29,5 +31,49 @@ class AuthController extends Controller
                 'user' => Auth::user()
             ]
         ]);
+    }
+
+    public function register(Request $request){
+
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required|string|min:8',
+            'password_confirmation' => 'required|string|same:password',
+        ]);
+        
+        if ($validator->fails()) {
+            return response()->json([
+                "code" => "ICF-003",
+                "success" => false,
+                "message" => "Registration Failed",
+                "errors" => $validator->errors(),
+                "result" => []
+            ], 400);
+        }
+        
+        try {
+            User::create([
+                "name" => $request->name,
+                "password" => bcrypt($request->password),
+                "email" => $request->email
+            ]);
+        
+            return response()->json([
+                "code" => "ICF-001",
+                "success" => true,
+                "message" => "Registration Successful",
+                "result" => []
+            ], 201);
+        
+        } catch (\Exception $e) {
+            return response()->json([
+                "code" => "ICF-003",
+                "success" => false,
+                "message" => "Registration Failed",
+                "error" => $e->getMessage(),
+                "result" => []
+            ], 500);
+        }
     }
 }
