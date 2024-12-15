9108f530d6bdbd715d349d56935bdc6a44a1f65f
ilhamhafidz404 <xxspanzie@gmail.com>
2024-12-15 22:20:38 +0700

    Change readme

--- app/Http/Controllers/API/DiscussionController.php
+++ app/Http/Controllers/API/DiscussionController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Models\Discussion;
 use Illuminate\Http\Request;
 
 class DiscussionController extends Controller
@@ -10,56 +11,19 @@ class DiscussionController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function __invoke(Request $request)
     {
-        //
-    }
+        $discussion = Discussion::create([
+            "forum_id" => $request->forum_id,
+            "message" => $request->body,
+            "user_id" => $request->user_id
+        ]);
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
+        return response()->json([
+            "code" => "ICF-001",
+            "success" => true,
+            "message" => "Kirim Diskusi Berhasil",
+            "result" => $discussion
+        ]);
     }
 }
